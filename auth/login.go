19e3218c6b9157c2eae4a73ae6caf9dfa8d460ee
Ryan S <wuuug@telegram>
2021-05-25 22:06:54 -0700

    move session cookie name to variable

--- auth/login.go
+++ auth/login.go
@@ -140,9 +140,11 @@ func DecryptAndValidate(es string, obj interface{}) error {
 
 var one_week time.Duration = time.Hour * 24 * 7
 
+const sessionCookieName string = "session"
+
 func Get(req *http.Request) *Session {
 	// fetch the session cookie and bail if it's unset.
-	session_blob, err := req.Cookie("session")
+	session_blob, err := req.Cookie(sessionCookieName)
 	if err != nil {
 		fmt.Println("no cookie at all")
 		return nil
@@ -176,7 +178,7 @@ func Put(w http.ResponseWriter, s *Session) {
 	}
 
 	var c http.Cookie
-	c.Name = "session"
+	c.Name = sessionCookieName
 	c.Value = value
 	c.Expires = s.SessionDate.Add(one_week)
 //	c.Secure = true
