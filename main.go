52fabfad39d2294298059291f06d586c95803def
Ryan S <wuuug@telegram>
2021-05-25 20:52:12 -0700

    add patreon oauth callback endpoint

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"io"
 	"net/http"
+	"net/url"
+	"time"
 	"html/template"
 	"encoding/json"
 	"io/ioutil"
@@ -63,6 +65,66 @@ func LinkAccount(w http.ResponseWriter, req *http.Request) {
 	go templateWrite(wp, templ, map[string]interface{} {
 		"State": state,
 		"Settings": GetClientSettings(),
+	})
+
+	io.Copy(w, rp)
+	rp.Close()
+}
+
+func LinkAccountPatreonReturn(w http.ResponseWriter, req *http.Request) {
+	q := req.URL.Query()
+	var state auth.PatreonState
+	err := auth.DecryptAndValidate(q.Get("state"), &state)
+	if err != nil {
+		// XXX logging should occur here, as it may indicate attempted abuse
+		// intentionally use the same, misleading error message as for expired state
+		http.Error(w, "Expired state", 400)
+		return
+	}
+
+	// XXX also check if state is expired
+
+	v := url.Values{}
+	settings := GetClientSettings()
+	v.Set("code", q.Get("code"))
+	v.Set("grant_type", "authorization_code")
+	v.Set("client_id", settings.PatreonApiClientId)
+	v.Set("client_secret", settings.PatreonApiClientSecret)
+	v.Set("redirect_uri", settings.PatreonLoginRedirect)
+	resp, err := http.PostForm("https://www.patreon.com/api/oauth2/token", v)
+	if err != nil {
+		http.Error(w, "Error connecting to patreon 1", 502)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	var s auth.Session
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, "Bad response from patreon 2", 502)
+		return
+	}
+
+	err = json.Unmarshal(b, &s.Patreon)
+	if err != nil {
+		http.Error(w, "Bad response from patreon 3", 502)
+		return
+	}
+
+	s.SessionDate = time.Now()
+
+	auth.Put(w, &s)
+	templ := template.Must(template.New("loggedinpage").Parse(
+`<html><head><title>Account Connected</title>
+{{if .HasState}}<meta http-equiv="refresh" content="3;url={{.ReturnTo}}" />{{end}}
+</head><body><p>Your patreon account is now connected.</p></body></html>`,
+	))
+
+	rp, wp := io.Pipe()
+	go templateWrite(wp, templ, map[string]interface{} {
+		"HasState": state.ReturnTo != "",
+		"ReturnTo": state.ReturnTo,
 	})
 
 	io.Copy(w, rp)
