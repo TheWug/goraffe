47386e4cc9f273a1c5140dfb20b8a22fe51b9838
Ryan S <wuuug@telegram>
2021-05-28 05:59:41 -0700

    Add object stubs

--- web/websocket.go
+++ web/websocket.go
@@ -2,3 +2,21 @@ package web
 
 import (
 )
+
+type RaffleHub struct {
+}
+
+type Action struct {
+}
+
+type Status struct {
+}
+
+type Lose struct {
+}
+
+type MasterStatus struct {
+}
+
+type Client struct {
+}
