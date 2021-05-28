9595bf89bb3ebee6b082a52960b6634fb526cb59
Ryan S <wuuug@telegram>
2021-05-28 06:35:00 -0700

    add database init method

--- store/store.go
+++ store/store.go
@@ -1,4 +1,17 @@
 package store
 
 import (
+	"database/sql"
 )
+
+var connection *sql.DB
+
+func Init(dburl string) error {
+	c, err := sql.Open("postgres", dburl)
+	if err != nil {
+		return err
+	}
+
+	connection = c
+	return nil
+}
