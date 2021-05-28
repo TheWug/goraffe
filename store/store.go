b671b454aeff70350be2993bd5c6f319e8d9386b
Ryan S <wuuug@telegram>
2021-05-28 06:48:18 -0700

    stub out raffle member functions

--- store/store.go
+++ store/store.go
@@ -24,3 +24,39 @@ type Entry struct {
 
 type Score struct {
 }
+
+func (this *Raffle) Status(user_id int) (*Entry, error) {
+	return nil, nil
+}
+
+func (this *Raffle) Enter(user_id int, display string) (bool, error) {
+	return false, nil
+}
+
+func (this *Raffle) Withdraw(user_id int, display string) (bool, error) {
+	return false, nil
+}
+
+func (this *Raffle) Disqualify(user_id int) (bool, error) {
+	return false, nil
+}
+
+func (this *Raffle) Undisqualify(user_id int) (*Entry, error) {
+	return nil, nil
+}
+
+func (this *Raffle) Open() (bool, error) {
+	return false, nil
+}
+
+func (this *Raffle) Close() (bool, error) {
+	return false, nil
+}
+
+func (this *Raffle) Cancel() (bool, error) {
+	return false, nil
+}
+
+func (this *Raffle) Draw() (*Entry, error) {
+	return nil, nil
+}
