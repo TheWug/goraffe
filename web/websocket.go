37f77aefc40ac80d3b6c30135997c2c1e72710cd
Ryan S <wuuug@telegram>
2021-05-28 06:01:24 -0700

    add body for rafflehub object

--- web/websocket.go
+++ web/websocket.go
@@ -1,9 +1,19 @@
 package web
 
 import (
+	"github.com/thewug/goraffe/store"
 )
 
 type RaffleHub struct {
+	Raffle  store.Raffle
+
+	Clients map[int][]*Client
+	Masters []*Client
+
+	Register chan *Client
+	Unregister chan *Client
+
+	Actions chan *Action
 }
 
 type Action struct {
